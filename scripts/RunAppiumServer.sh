470a488ae573b15c30e2a460107bef22ddd6c33b
officialtpss <officialtpss@gmail.com>
2023-08-25 16:23:42 +0530

    new plugin changes

--- scripts/RunAppiumServer.sh
+++ scripts/RunAppiumServer.sh
@@ -5,7 +5,6 @@ appium driver install uiautomator2
 appium driver install xcuitest
 appium driver install espresso
 appium driver install --source=npm appium-flutter-driver
-appium plugin install --source=npm appium-reporter-plugin
 
 appium -v
-appium --log --use-plugins=appium-reporter-plugin appium.log &>/dev/null &
+appium --log appium.log &>/dev/null &
