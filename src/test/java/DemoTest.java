3b81584ff115d1e5c8fba8fc084ef2b729ba903a
officialtpss <officialtpss@gmail.com>
2023-08-28 11:51:46 +0530

    minor changes

--- src/test/java/DemoTest.java
+++ src/test/java/DemoTest.java
@@ -1,9 +1,8 @@
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -11,21 +10,39 @@ import org.testng.annotations.Test;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import io.appium.java_client.ios.IOSDriver;
+
+
 public class DemoTest {
 
     WebDriver driver;
+    String mHost="http://localhost:4723/wd/hub";
 
     @BeforeClass
     public void setUp() throws MalformedURLException {
         System.out.println("<<<<<<<<<<<<<<<<<<<<<<Test Start>>>>>>>>>>>>>>>>>>>>");
-        DesiredCapabilities caps = new DesiredCapabilities();
-        caps.setCapability("automationName" , "UiAutomator2");
-        caps.setCapability("platformName" , "Android");
-        caps.setCapability("appium:platformVersion" , "9");
-        caps.setCapability("appium:deviceName" , "Android Emulator");
-        caps.setCapability("appium:app" ,
-                System.getProperty("user.dir")+"/apps/app-debug.apk");
-        driver = new AndroidDriver(new URL("http://localhost:4723"), caps);
+
+
+        DesiredCapabilities iosCapabilities = new DesiredCapabilities();
+        iosCapabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "iOS");
+        iosCapabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "15.4");
+        iosCapabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "iOS 15.4");
+        iosCapabilities.setCapability(MobileCapabilityType.APP, System.getProperty("user.dir")+"/apps/BStackSampleApp.ipa");
+        iosCapabilities.setCapability("automationName", "XCUITest"); // Automation name for iOS
+
+        driver = new IOSDriver<>(new URL(mHost), iosCapabilities);
+
+
+//
+//        // Create an Android driver using Android desired capabilities
+//        DesiredCapabilities androidCapabilities = new DesiredCapabilities();
+//        androidCapabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+//        androidCapabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "11");
+//        androidCapabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Pixel 4");
+//        androidCapabilities.setCapability(MobileCapabilityType.APP, System.getProperty("user.dir")+"/apps/app-debug.apk");
+//        androidCapabilities.setCapability("automationName", "UiAutomator2"); // Automation name for Android
+//        driver = new AndroidDriver<>(new URL(mHost), androidCapabilities);
+
     }
 
     @Test
