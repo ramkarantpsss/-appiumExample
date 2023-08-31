0452ba60bb6bfa8e99668f24d935a5398426db77
officialtpss <officialtpss@gmail.com>
2023-08-31 16:42:34 +0530

    minor changes

--- scripts/RunAppiumServer.sh
+++ scripts/RunAppiumServer.sh
@@ -2,9 +2,11 @@
 set -ex
 npm install -g appium
 npm install @appium/doctor --location=global
-appium driver install appium-xcuitest-driver
-appium driver install appium-flutter-driver
-
+appium driver install uiautomator2
+appium driver install chromium
+appium driver install xcuitest
+appium driver install --source=npm appium-flutter-driver
+ls /Users/runner/.appium
 appium -v
 appium driver list --installed
 appium-doctor --ios
