ec76850abb7fed67b4bf8e998160d3fe39fdaebf
officialtpss <officialtpss@gmail.com>
2023-08-31 13:43:30 +0530

    minor changes

--- scripts/RunAppiumServer.sh
+++ scripts/RunAppiumServer.sh
@@ -3,7 +3,6 @@ set -ex
 npm install -g appium@next
 appium driver install uiautomator2
 appium driver install xcuitest
-appium driver install espresso
 appium driver install --source=npm appium-flutter-driver
 
 appium -v
