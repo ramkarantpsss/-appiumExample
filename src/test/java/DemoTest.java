f75f91e298c9a51f9851ad520b7d562159c2dab6
iamparamdev2050 <91934080+iamparamdev2050@users.noreply.github.com>
2023-08-31 10:12:27 +0530

    caps change device name

--- src/test/java/DemoTest.java
+++ src/test/java/DemoTest.java
@@ -1,6 +1,6 @@
-import io.appium.java_client.android.AndroidDriver;
+
+import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.AfterClass;
@@ -16,41 +16,29 @@ import io.appium.java_client.ios.IOSDriver;
 public class DemoTest {
 
     WebDriver driver;
-    String mHost="http://localhost:4723/wd/hub";
+    String mHost="http://localhost:4723";
 
     @BeforeClass
     public void setUp() throws MalformedURLException {
         System.out.println("<<<<<<<<<<<<<<<<<<<<<<Test Start>>>>>>>>>>>>>>>>>>>>");
 
 
-    //    DesiredCapabilities iosCapabilities = new DesiredCapabilities();
-   //     iosCapabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "iOS");
-   //     iosCapabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "15.4");
-    //    iosCapabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "TestiPhoneX");
-   //     iosCapabilities.setCapability(MobileCapabilityType.APP, System.getProperty("user.dir")+"/apps/BStackSampleApp.ipa");
-   //     iosCapabilities.setCapability("automationName", "XCUITest"); // Automation name for iOS
-    //    iosCapabilities.setCapability("isHeadless", true);
-    //    driver = new IOSDriver<>(new URL(mHost), iosCapabilities);
-
-
-
-        // Create an Android driver using Android desired capabilities
-       DesiredCapabilities androidCapabilities = new DesiredCapabilities();
-        androidCapabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
-        androidCapabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "11");
-        androidCapabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Nexus 6");
-        androidCapabilities.setCapability(MobileCapabilityType.APP, System.getProperty("user.dir")+"/apps/app-debug.apk");
-        androidCapabilities.setCapability("automationName", "UiAutomator2"); // Automation name for Android
-        driver = new AndroidDriver<>(new URL(mHost), androidCapabilities);
+        DesiredCapabilities iosCapabilities = new DesiredCapabilities();
+        iosCapabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "iOS");
+        iosCapabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "14.2");
+        iosCapabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "iPhone 11");
+        System.out.println("<<<<<<<<<<<<<<<<<<<<<<Set .app file>>>>>>>>>>>>>>>>>>>>");
+        iosCapabilities.setCapability(MobileCapabilityType.APP, System.getProperty("user.dir")+"/apps/Runner.app");
+        iosCapabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "XCUITest");
+        System.out.println("<<<<<<<<<<<<<<<<<<<<<<Set iosCapabilities start>>>>>>>>>>>>>>>>>>>>");
+        driver= new IOSDriver(new URL(mHost), iosCapabilities);
+        System.out.println("<<<<<<<<<<<<<<<<<<<<<<Set iosCapabilities Done >>>>>>>>>>>>>>>>>>>>");
 
     }
 
     @Test
     public void test_Appium() {
-        driver.findElement(By.id("buttonFive")).click();
-        driver.findElement(By.id("buttonAdd")).click();
-        driver.findElement(By.id("buttonFour")).click();
-        driver.findElement(By.id("buttonEqual")).click();
+        System.out.println("<<<<<<<<<<<<<<<<<<<<<<Between Test>>>>>>>>>>>>>>>>>>>>");
     }
     @AfterClass
     public void tearDown()    {
